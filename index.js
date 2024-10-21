641b60d502c112baa3ac93cd70e37b101f6618c8
Prashant Rastogi <publicidforpeople>
2024-10-21 19:32:33 +0530

    add dataTypes

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ console.log("Here is JavaScript Code: ");
 
 let text = "The Quick Brown Fox Jumps Over The Lazy Dog.";
 console.log(text);
+
+let dataTypes = {
+  null: null,
+  true: true,
+  number: 123,
+  false: false,
+  undefined: undefined,
+  string: "text string",
+  symbol: Symbol("This is symbol"),
+};
+// let a = Array.from(dataTypes).forEach((element) => {
+// //   return typeof element;
+// });
+
+console.log(dataTypes);
