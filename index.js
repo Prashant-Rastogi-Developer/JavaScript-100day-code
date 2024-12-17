fa837b4d338836a860ce299b4a7236efe984ab8c
Prashant Rastogi <publicidforpeople>
2024-12-17 18:07:02 +0530

    improve switch case code in month change into day

--- index.js
+++ index.js
@@ -237,19 +237,28 @@ toCamelCase(" Java script hello world"); // JavaScriptHelloWorld
 toCamelCase("get element by id"); // getElementById
 // --------------------------------------------
 // switch case values
-let month = 3;
-switch (month) {
+let day = new Date().getDay();
+switch (day) {
   case 1:
-    "this month of January";
+    "Monday";
     break;
   case 2:
-    "this month of February";
+    "Tuesday";
     break;
   case 3:
-    "this month of March";
+    "Wednesday";
+    break;
+  case 3:
+    "Thursday";
+    break;
+  case 3:
+    "Friday";
+    break;
+  case 3:
+    "Saturday";
     break;
   default:
-    "otherwise return any value";
+    "wrong input";
     break;
 }
 // --------------------------------------------
